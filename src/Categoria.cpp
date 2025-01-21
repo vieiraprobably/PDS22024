c6450d16f67812f12a101a43bf12bf2b574b9942
felipesvm <federt49@gmail.com>
2025-01-21 14:43:03 -0300

    Classe base Produto + Enum Categoria

---
+++ src/Categoria.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+enum class Categoria {
+    Carne,
+    Verdura,
+    ProdutoAnimal,
+    ProdutoLimpeza,
+    Enlatados,
+    Laticinios,
+    Massas,
+    Bebidas,
+    HigienePessoal,
+    Padaria
+};
