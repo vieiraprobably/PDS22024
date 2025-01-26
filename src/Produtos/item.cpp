a85edd0f806f64d3ec493564df0a50b224c4d0ab
joaoPedroRG <jprgamalol@gmail.com>
2025-01-26 15:45:35 -0300

    Update item.cpp

--- src/Produtos/item.cpp
+++ src/Produtos/item.cpp
@@ -4,7 +4,7 @@
 class item{
     public:
     void imprimeProdutos(){}
-    bool modificaUnidadeDe(std::string Nome, int quantia, bool mod){}
+    bool modificaUnidadeDe(std::string Nome, int quantia, bool mod){}//mod = 1 -> soma, mod = 0 -> subtracao
     item* encontraUnidade(std::string Nome){}
     item* criaUnidade(std::string Nome, int Quantidade, float Preco){}
     item* criaUnidade(std::string Nome, float Preco){}
