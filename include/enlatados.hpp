105e04204cfc858b8b7e92e10f2216b659dd2ea7
vieiraprobably <cesarfilippini@icloud.com>
2025-01-28 21:33:13 -0300

    Create enlatados.hpp

---
+++ include/enlatados.hpp
@@ -0,0 +1,26 @@
+#ifndef ENLATADOS_HPP
+#define ENLATADOS_HPP
+
+#include "item.hpp"
+#include <string>
+#include <vector>
+
+class Enlatados : public item {
+private:
+    struct Produto {
+        std::string nome;
+        int quantidade;
+        float preco;
+    };
+
+    std::vector<Produto> produtos;
+
+public:
+    void imprimeProdutos() override;
+    bool modificaUnidadeDe(std::string Nome, int quantia, bool mod) override;
+    item* encontraUnidade(std::string Nome) override;
+    item* criaUnidade(std::string Nome, int Quantidade, float Preco) override;
+    item* criaUnidade(std::string Nome, float Preco) override;
+};
+
+#endif
