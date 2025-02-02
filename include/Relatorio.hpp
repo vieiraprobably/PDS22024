2574fd8b9b6f600b6ed23a6f29744cc2ae8760b5
Ana <analuizagoncalvesleopoldino@gmail.com>
2025-02-02 17:38:44 -0300

    Atualizando Relatorio.hpp

--- include/Relatorio.hpp
+++ include/Relatorio.hpp
@@ -1,13 +1,20 @@
 #ifndef RELATORIO_HPP
 #define RELATORIO_HPP
 
+#include "include/PersistenciaDeDados.hpp"
+
 class Relatorio {
+
 private:
-public:
-    Relatorio();
-    ~Relatorio();
+    PersistenciaDeDados& persistencia;
     
-    void gerarRelatorio();
+    void imprimirCabecalho() const;
+    void imprimirLinha(const std::string& categoria, int quantidade, double valorTotal) const;
+    void imprimirTotalGeral(int totalProdutos, double valorTotal) const;
+
+public:
+    Relatorio(PersistenciaDeDados& persistencia);
+    void gerarRelatorio() const;
 };
 
 #endif
