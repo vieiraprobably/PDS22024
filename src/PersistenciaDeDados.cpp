0dbd8ec6754fd84da05e379401f342c36f7b162e
Ana <analuizagoncalvesleopoldino@gmail.com>
2025-02-02 04:12:51 -0300

    Arrumando ids de fornecedores do arquivo Verduras.csv estavam errados

---
+++ src/PersistenciaDeDados.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+class PersistenciaDeDados {
+private:
+    std::string caminhoArquivo;
+
+public:
+    
+    PersistenciaDeDados(const std::string& caminho) : caminhoArquivo(caminho) {}
+
+    
+    void adicionarProduto(const std::string& linha) {
+        std::ofstream arquivo(caminhoArquivo, std::ios::app);
+        if (arquivo.is_open()) {
+            arquivo << linha << "\n";
+            arquivo.close();
+        } else {
+            std::cerr << "Erro ao abrir o arquivo para escrita." << std::endl;
+        }
+    }
+
+    
+    std::string buscarProduto(const std::string& palavra) {
+        std::ifstream arquivo(caminhoArquivo);
+        std::string linha;
+
+        if (arquivo.is_open()) {
+            while (std::getline(arquivo, linha)) {
+                if (linha.find(palavra) != std::string::npos) {
+                    arquivo.close();
+                    return linha;
+                }
+            }
+            arquivo.close();
+        } else {
+            std::cerr << "Erro ao abrir o arquivo para leitura." << std::endl;
+        }
+        return ""; 
+    }
+
+    
+    void apagarProduto(const std::string& palavra) {
+        std::ifstream arquivo(caminhoArquivo);
+        std::vector<std::string> linhas;
+        std::string linha;
+
+        if (arquivo.is_open()) {
+            while (std::getline(arquivo, linha)) {
+                if (linha.find(palavra) == std::string::npos) {
+                    linhas.push_back(linha);
+                }
+            }
+            arquivo.close();
+        } else {
+            std::cerr << "Erro ao abrir o arquivo para leitura." << std::endl;
+            return;
+        }
+
+        std::ofstream arquivoSaida(caminhoArquivo);
+        if (arquivoSaida.is_open()) {
+            for (const auto& l : linhas) {
+                arquivoSaida << l << "\n";
+            }
+            arquivoSaida.close();
+        } else {
+            std::cerr << "Erro ao abrir o arquivo para escrita." << std::endl;
+        }
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include "PersistenciaDeDados.cpp"
-#include "GerenciadorDeEstoque.cpp"
-#include "MenuInterativo.cpp"
 
   int main() {
     MenuInterativo menu(GerenciadorDeEstoque gerenciador(PersistenciaDeDados persistencia("dados.csv")));
