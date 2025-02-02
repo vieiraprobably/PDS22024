8a4562d779917e5350173eb4f716134427e058f8
Ana <analuizagoncalvesleopoldino@gmail.com>
2025-02-02 17:34:35 -0300

    Atualizando os métodos Admin.hpp

--- include/Admin.hpp
+++ include/Admin.hpp
@@ -4,29 +4,30 @@
 #include <string>
 
 class Admin {
+
 private:
     std::string nome;
-    std::string login;
+    std::string username;
     std::string senha;
     std::string palavraSecreta;
 
 public:
-    Admin(std::string nome, std::string login, std::string senha, std::string palavraSecreta);
+    Admin(const std::string& nome, const std::string& username, const std::string& senha, const std::string& palavraSecreta);
     ~Admin();
     
     std::string getNome() const;
-    std::string getLogin() const;
+    std::string getUsername() const;
     std::string getSenha() const;
     std::string getPalavraSecreta() const;
 
     void setNome(const std::string& nome);
-    void setLogin(const std::string& login);
+    void setUsername(const std::string& username);
     void setSenha(const std::string& senha);
     void setPalavraSecreta(const std::string& palavraSecreta);
 
-    bool autenticarUsuario(std::string login, std::string senha);
-    void cadastrarUsuario(std::string nome, std::string login, std::string senha, std::string palavraSecreta);
-    void recuperarSenha(std::string login);
+    bool autenticarUsuario(const std::string& username, const std::string& senha, PersistenciaDeDados& persistencia);
+    bool cadastrarUsuario(const std::string& nome, const std::string& username, const std::string& senha, const std::string& palavraSecreta, PersistenciaDeDados& persistencia);
+    bool recuperarSenha(const std::string& nome, const std::string& username, const std::string& novaSenha, const std::string& palavraSecreta, PersistenciaDeDados& persistencia);
 };
 
 #endif
